73ccb206855a1b0decff7327bc63f90cb5dec3b6
Claus Polanka <claus.polanka@chello.at>
2015-03-16 23:54:55 +0100

    [Red] Level 5 example 2.

--- Harvester.Domain.Test/HarvesterTest.cs
+++ Harvester.Domain.Test/HarvesterTest.cs
@@ -133,6 +133,7 @@ namespace Harvester.Domain.Test
         }
 
         [TestCase(5, 4, 1, 1, OST, SERPENTINE, 2, "1 5 2 6 3 7 4 8 16 12 15 11 14 10 13 9 17 18 19 20")]
+        [TestCase(5, 4, 4, 1, OST, CIRCULAR, 2, "13 17 14 18 15 19 16 20 8 4 7 3 6 2 5 1 9 10 11 12")]
         public void Level_5_spec_examples(
             int rows,
             int cols,
