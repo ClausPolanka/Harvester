5a621f7d0b6c79cf9e7a8869285e84b2fded6948
Claus Polanka <claus.polanka@chello.at>
2015-03-29 19:42:38 +0200

    [Red] Odd numbers starting in first row first column going south.

--- Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
+++ Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
@@ -25,7 +25,7 @@ namespace Harvester.Domain.UnitTest
         //   7  8  9
         //  10 11 12* ←
         [TestCase(4, 3, 4, 3, "W", "12 11 10 1 2 3 9 8 7 4 5 6")]
-        public void Even_number_of_rows(
+        public void Even_number_of_rows_east_and_west(
             int nrOfRows,
             int nrOfCols,
             int startRow,
@@ -64,7 +64,29 @@ namespace Harvester.Domain.UnitTest
         //  10 11 12
         //  13 14 15* ←
         [TestCase(5, 3, 5, 3, "W", "15 14 13 1 2 3 12 11 10 4 5 6 9 8 7")]
-        public void Odd_number_of_rows(
+        public void Odd_number_of_rows_east_and_west(
+            int nrOfRows,
+            int nrOfCols,
+            int startRow,
+            int startCol,
+            string direction,
+            string expected)
+        {
+            var sut = new CircularHarvester(nrOfRows, nrOfCols, direction, width: 1);
+
+            var actual = sut.Harvest(startRow, startCol);
+
+            Assert.That(actual, Is.EqualTo(expected), "plot numbers");
+        }
+
+        //  ↓
+        //  1*  2  3
+        //  4   5  6
+        //  7   8  9
+        // 10  11 12
+        // 13  14 15
+        [TestCase(5, 3, 1, 1, "S", "1 4 7 10 13 15 12 9 6 3 2 5 8 11 14")]
+        public void Odd_number_of_rows_north_and_south(
             int nrOfRows,
             int nrOfCols,
             int startRow,
