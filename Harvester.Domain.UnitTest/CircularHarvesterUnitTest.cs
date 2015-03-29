e46d57f6269e225600020441c064b5c62c031216
Claus Polanka <claus.polanka@chello.at>
2015-03-29 20:01:23 +0200

    [Green] Odd numbers starting in last row last column going north.

--- Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
+++ Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
@@ -100,6 +100,13 @@ namespace Harvester.Domain.UnitTest
         // 13* 14 15
         //  ↑
         [TestCase(5, 3, 5, 1, "N", "13 10 7 4 1 3 6 9 12 15 14 11 8 5 2")]
+        //  1  2  3
+        //  4  5  6
+        //  7  8  9
+        // 10 11 12
+        // 13 14 15*
+        //        ↑
+        [TestCase(5, 3, 5, 5, "N", "15 12 9 6 3 1 4 7 10 13 14 11 8 5 2")]
         public void Odd_number_of_rows_north_and_south(
             int nrOfRows,
             int nrOfCols,
