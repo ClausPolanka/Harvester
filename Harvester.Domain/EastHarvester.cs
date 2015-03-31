ea4d2a5ca66ef995bf0d5895dd03c9e8e4cc3f0d
Claus Polanka <claus.polanka@chello.at>
2015-03-31 19:51:47 +0200

    [Refactoring] Odd number of rowsand harvester width of two going west starting in last row.

---
+++ Harvester.Domain/EastHarvester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Harvester.Domain
+{
+    public class EastHarvester : HarvesterDirection
+    {
+        private readonly int rows;
+        private readonly int cols;
+        private readonly int width;
+        private HarvesterMode harvesterMode;
+
+        public EastHarvester(int rows, int cols, int width, HarvesterMode harvesterMode)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.width = width;
+            this.harvesterMode = harvesterMode;
+        }
+
+        public string Harvest(int startRow, int startCol)
+        {
+            var plotRows = new PlotRowCreator().CreatePlotRows(rows, cols);
+
+            if (width == 2)
+                plotRows = plotRows.Merge_two_rows_starting_top_left();
+
+            return harvesterMode.Harvest(startRow, plotRows, "O");
+        }
+    }
+}
---
+++ Harvester.Domain/WestHarvester.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Harvester.Domain
+{
+    public class WestHarvester : HarvesterDirection
+    {
+        private readonly int rows;
+        private readonly int cols;
+        private readonly int width;
+        private HarvesterMode harvesterMode;
+
+        public WestHarvester(int rows, int cols, int width, HarvesterMode harvesterMode)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            this.width = width;
+            this.harvesterMode = harvesterMode;
+        }
+
+        public string Harvest(int startRow, int startCol)
+        {
+            var plotRows = new PlotRowCreator().CreatePlotRows(rows, cols);
+
+            if (width == 2)
+            {
+                if (startRow == 1)
+                    plotRows = plotRows.Merge_two_rows_starting_top_left();
+                else
+                    plotRows = plotRows.Merge_two_rows_starting_bottom_left();
+            }
+
+            return harvesterMode.Harvest(startRow, plotRows, "W");
+        }
+    }
+}
