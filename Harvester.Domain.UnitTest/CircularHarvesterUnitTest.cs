1880338ce7d44a80d2625e5331a4bbc62f1dd0bd
Claus Polanka <claus.polanka@chello.at>
2015-04-01 15:56:19 +0200

    [Red] Odd number of cols going south via serpentine mode starting top left with havesting width of two.

--- Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
+++ Harvester.Domain.UnitTest/CircularHarvesterUnitTest.cs
@@ -165,35 +165,12 @@ namespace Harvester.Domain.UnitTest
             Assert.That(actual, Is.EqualTo(expected), "plot numbers");
         }
 
-        //  ↓   ↓
-        //  1*  2  3
-        //  4   5  6
-        //  7   8  9
-        // 10  11 12
-        // 13  14 15
-        //[TestCase(5, 3, 1, 1, "S", "1 4 7 10 13 15 12 9 6 3 2 5 8 11 14")]
-        //        ↓
-        //  1  2  3*
-        //  4  5  6
-        //  7  8  9
-        // 10 11 12
-        // 13 14 15
-        //[TestCase(5, 3, 1, 3, "S", "3 6 9 12 15 13 10 7 4 1 2 5 8 11 14")]
-        //  1   2  3
-        //  4   5  6
-        //  7   8  9
-        // 10  11 12
-        // 13* 14 15
-        //  ↑
-        //[TestCase(5, 3, 5, 1, "N", "13 10 7 4 1 3 6 9 12 15 14 11 8 5 2")]
-        //  1  2  3
-        //  4  5  6
-        //  7  8  9
-        // 10 11 12
-        // 13 14 15*
-        //        ↑
-        //[TestCase(5, 3, 5, 5, "N", "15 12 9 6 3 1 4 7 10 13 14 11 8 5 2")]
-        public void Odd_number_of_rows_north_and_south_and_a_width_of_two(
+        // ↓  ↓
+        // 1* 2 3 4 5
+        // 6  7 8 9 10
+        [TestCase(5, 3, 1, 1, "S", "2 1 7 6 9 10 4 5 3 8")]
+        
+        public void Odd_number_of_cols_north_and_south_and_a_width_of_two(
             int nrOfRows,
             int nrOfCols,
             int startRow,
